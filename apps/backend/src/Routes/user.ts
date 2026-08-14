dfa6f00d48e229a15bccf428200c964a174c0dc5
n3rddev <n3rddev.code@gmail.com>
2026-08-14 22:28:20 +0530

    user route and zod schema

---
+++ apps/backend/src/Routes/user.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { signupSchema } from "../Types/zodSchema.js";
+import {userModel, interviewModel, messageModel} from "db"
+
+const userRouter: Router = Router();
+
+userRouter.post("/signup", async(req, res)=>{
+    const {username, password, role} = req.body;
+    
+    if(!username || !password || !role){
+        return res.status(401).json({
+            message: "enough credentials not found"
+        })
+    }
+
+    try {
+        const validateCredentials = signupSchema.safeParse({username, password, role})
+
+        if(!validateCredentials.success){
+            return res.status(402).json({
+                message: "iinvalid format of credentials"
+            })
+        }
+
+        const checkUsername =await userModel.findOne({
+            username
+        })
+
+        if(!checkUsername){
+            const resp = await userModel.create({
+                username,
+                password,
+                role
+            })
+            return res.status(200).json({
+                message: "user on boarded",
+                id: resp._id
+            })
+        }
+
+        return res.status(400).json({
+            message: "username already exists"
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "internal server error"
+        })
+    }
+})
+
+export default userRouter;
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import env from "dotenv";
 import mongoose from "mongoose";
-import {userModel, interviewModel, messageModel} from "db"
+import userRouter from "./Routes/user.js";
 
 env.config()
 
 const app = express();
+
+app.use(express.json());
+app.use("/user", userRouter)
 
 app.get("/", (req, res)=>{
     return res.status(200).json({
@@ -19,7 +22,7 @@ const PORT = process.env.PORT || 3000;
 
 
 const startServer = async()=>{
-    await mongoose.connect("localhost:27017")
+    await mongoose.connect("mongodb://localhost:27017/db")
     app.listen(PORT ,()=>{
         console.log("listening on ", PORT)
     })
